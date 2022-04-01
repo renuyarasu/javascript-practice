95b3e3d9de0c143f72f0c0076e7479dee2a06659
Renuka Prasad <renu.yarasu@gmail.com>
2022-04-01 21:43:04 +0530

    Practice: 06

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-// JavaScript Practice: 05
+// JavaScript Practice: 06
 console.clear();
 
-console.log(str); // undefined
-var str = 'JavaScript';
+let
+    a = true,
+    b = true,
+    c = false;
+
+let X = c ? (b ? 'VedaGna' : 'Yarasu') : (a ? 'Gnapika' : 'Yarasu');
+console.log(X); // Gnapika
+
+
+// Tip - when using the ternary operator, add parentheses (but not as much as in the last option above).
+
